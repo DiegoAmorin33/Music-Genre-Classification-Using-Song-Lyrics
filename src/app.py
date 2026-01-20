7b351160ad97ffbfd514d761247a4f16aa95dcd6
DiegoAmorin33 <amorinoficial33@gmail.com>
2026-01-20 03:06:52 +0000

    DatosAnadidos2

--- src/app.py
+++ src/app.py
@@ -1,4 +1,19 @@
 from utils import db_connect
 engine = db_connect()
 
-# your code here
+bad_lines = []
+
+def bad_line_handler(line):
+    bad_lines.append(line)
+    return None   # None = skip la línea
+
+df = pd.read_csv(
+    "lyrics,label.txt",
+    sep=",",
+    engine="python",
+    on_bad_lines=bad_line_handler
+)
+
+print("Cantidad de líneas malas:", len(bad_lines))
+print("Ejemplo de línea mala:")
+print(bad_lines[0])
